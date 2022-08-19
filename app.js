28e682f92145edd8364d8437612c623aaeeabda3
bilaly <billycsk@gmail.com>
2022-08-19 22:17:35 +0200

    Ajout de style

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ let btn = document.getElementById('btn');
 
 btn.addEventListener('click', function (e) {
     let getValueInput = document.getElementById('input').value;
-    console.log(getValueInput);
+    //console.log(getValueInput);
     let taskValue = []
     taskValue.unshift(getValueInput)
     let liste = '<ul>';
     for (const getTaskItem of taskValue) {
-        liste += ` <li> ${getTaskItem} </li>`
+        liste += `<li> ${getTaskItem} </li>`
     }
     liste += '</ul>'
-
     document.querySelector('.tache').innerHTML += liste
-})
+});
