711c6a27cded70855f866e906bb97f60df41fcba
bilaly <billycsk@gmail.com>
2022-08-19 18:28:55 +0200

    task type integer => array

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ let btn = document.getElementById('btn');
 
 btn.addEventListener('click', function (e) {
     let getValueInput = document.getElementById('input').value;
-    //console.log(getValueInput);
-
+    console.log(getValueInput);
+    let taskValue = []
+    taskValue.unshift(getValueInput)
     let liste = '<ul>';
-    liste += ` <li> ${getValueInput} </li>`
-    liste += '</ul>';
+    for (const getTaskItem of taskValue) {
+        liste += ` <li> ${getTaskItem} </li>`
+    }
+    liste += '</ul>'
 
-    document.querySelector('.tache').innerHTML += liste;
+    document.querySelector('.tache').innerHTML += liste
 })
